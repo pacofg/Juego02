1497f61daee9101413d79370ed8f9cbb120eee02
Paco <mi@casa.es>
2022-02-28 17:36:05 +0100

    Agregada clase persona

--- Carta.py
+++ Carta.py
@@ -1,3 +1,9 @@
+# Carta
+    # Tiene
+        # - palo (exto)
+        # - numero (entero)
+    # + imprimir(): Imprime el numero y el palo de la carta
+
 class Carta:
     
     def __init__(self, numero, palo):
