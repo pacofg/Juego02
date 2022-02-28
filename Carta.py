1ab715e6e611e28d6c41ab655affde03bc5e847c
Paco <mi@casa.es>
2022-02-28 17:21:58 +0100

    Carta parametrizada

--- Carta.py
+++ Carta.py
@@ -1,12 +1,15 @@
 class Carta:
     
-    def __init__(self):
-        self.palo = "Espadas"
-        self.numero = 8
+    def __init__(self, numero, palo):
+        self.palo = palo
+        self.numero = numero
     
     def imprimir(self):
         print(self.numero, "de", self.palo)
 
  # Esto genera una instancia d la clase Carta
-carta1 = Carta()
+carta1 = Carta(10, "Diamantes")
+carta1.imprimir()
+
+carta1 = Carta(7, "Espadas")
 carta1.imprimir()
