6d0d6ac09ec49e0e582537bd8d60f47741a1a22f
Paco <mi@casa.es>
2022-03-07 18:03:45 +0100

    Actualizado InterfazJuegoCartas: dibujar_carta

--- InterfazJuegoCartas.py
+++ InterfazJuegoCartas.py
@@ -1,5 +1,6 @@
 from email.mime import image
 from tkinter import *
+from turtle import width
 
 class InterfazJuegoCartas:
     
@@ -15,11 +16,27 @@ class InterfazJuegoCartas:
          # Cargamos el fondo de la ventana
         self.dibujar_fondo()
                 
+         # Fondo de la carta como un poligono de fondo blanco y borde negro
+        self.dibujar_poligono([50,170,170,50], [55,55,230,230], fill='#FFFFFF', width=2, outline='#000000')
+         # Dibujamos una carta
+        self.dibujar_carta()
+                
          # Mostramos la ventan
         self.ventana.mainloop()
          # Cualquier cosa que tengamos despues ya no se vera
 
-
+    def dibujar_poligono(self, x, y, **args):
+        puntos = []
+        for i in range(len(x)):
+            puntos.append(x[i])
+            puntos.append(y[i])
+        
+         # Punto inicial para cerra rel poligono
+        puntos.append(x[0])
+        puntos.append(y[0])
+        
+        return self.canvas.create_polygon(puntos, **args)    
+        
     def dibujar_fondo(self):
          # Cargamos el archivo con la imagen a pintar
         fondo = PhotoImage(file='juegoCartas/fondo-verde.png')
@@ -42,7 +59,12 @@ class InterfazJuegoCartas:
          # Lo incorpora al canvas, jugando con la posicion para dejarlo bien
         self.canvas.create_image(1100,500, image=adornos)
         
-
+    def dibujar_carta(self, x=110, y=143, imagen='juegoCartas/2_de_espadas.png'):
+        imagen_canvas = PhotoImage(file=imagen)
+        imagen_canvas = imagen_canvas.subsample(2)
+        imagen_label = Label(image=imagen_canvas)
+        imagen_label.image = imagen_canvas
+        self.canvas.create_image(x, y, image=imagen_canvas)
 
 def main():
     interfaz = InterfazJuegoCartas()
